cf0ab234d606439d295b9d67e13998e95425e891
Reyner Veliz <reyner@reynerveliz.com>
2024-09-22 04:30:52 -0600

    Changes to oxygen system

--- pages/oxygen.py
+++ pages/oxygen.py
@@ -45,3 +45,12 @@ with st.expander("What FAR requirements for crew oxygen in a pressurizaed/unpres
     - {hightlight}[**12,500 ft - 14,000 ft, 30 min max**] the oxygen is required
 """)
      
+with st.expander("FAR 91.211 summary:?"):
+     st.write(f"""
+- {hightlight}[**Above FL410:**] 
+    - One pilot must use an oxygen mask at all times
+- {hightlight}[**Between FL350 and FL410**]
+    - The remaining pilot must wear an oxygen mask if the other pilot leaves the controls
+- {hightlight}[**Below FL350**] 
+    - Oxygen masks must be readily available for use, but continuous wear is not required if one pilot leaves the controls.  Both pilots must have quick-don masks available
+""")
